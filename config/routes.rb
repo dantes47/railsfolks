e3f2f021cee2b1fa648b5f2f262548037bbdd5a9
Danny <dantes817@gmail.com>
2020-11-17 23:07:31 +0100

    last touches/ was added a footer/ a bad one, with links though..

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :tasks
+
   # get 'home/index'
-
   get 'home/about'
+  # To whom who wants to see the Thinker-Guy >> uncomment route bellow..
 
-  root 'home#index'
+  # root 'home#index'
+  root 'tasks#index'
 end
