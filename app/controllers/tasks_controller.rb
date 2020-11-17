8abc8c21694e227e703e4ae2729e5f4c89c602e2
Danny <dantes817@gmail.com>
2020-11-17 20:10:47 +0100

    was added assoc/ new db-table for assoc users<>tasks for every user/ added IDs

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -69,6 +69,6 @@ class TasksController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def task_params
-      params.require(:task).permit(:first_item, :created, :title, :content)
+      params.require(:task).permit(:first_item, :created, :title, :content, :user_id)
     end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_16_172329) do
+ActiveRecord::Schema.define(version: 2020_11_17_174829) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,8 @@ ActiveRecord::Schema.define(version: 2020_11_16_172329) do
     t.text "content"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_tasks_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
